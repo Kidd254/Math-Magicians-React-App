e94d0895ae17ca3639fe043117886c06995726d2
Kidd254 <lorenkioko@gmail.com>
2023-07-05 23:24:29 +0300

    fix errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import calculate from '../logic/calculate';
-import OrangeBtn from './OrangeButton';
-import GrayBtn from './GrayButton';
 import GrayButton from './GrayButton';
 import OrangeButton from './OrangeButton';
 
