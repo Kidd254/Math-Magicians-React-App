af7f93098fb8e02fde2f0fefbc731131be5273d1
Kidd254 <lorenkioko@gmail.com>
2023-07-06 19:05:14 +0300

    add API key

--- src/components/hook.js
+++ src/components/hook.js
@@ -4,21 +4,32 @@ function DisplayQuotes() {
   const [data, setData] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const apiKey = 'D5q8niB700jxi57PqkJC6Q==D1xzw7ooFWM5nyYD';
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const results = await fetch('https://api-ninjas.com/api/quotes');
-        const json = await results.json();
-        setData(json);
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+          headers: {
+            'X-Api-Key': apiKey,
+          },
+        });
+        if (response.ok) {
+          const json = await response.json();
+          setData(json);
+        } else {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
       } catch (error) {
         setHasError(true);
       }
       setIsLoading(false);
     };
+
     fetchData();
-  }, [data, setData]);
+  }, [setData, setIsLoading]);
+
   if (hasError) {
     return (
       <article>
@@ -26,6 +37,7 @@ function DisplayQuotes() {
       </article>
     );
   }
+
   if (isLoading) {
     return (
       <article>
@@ -33,11 +45,21 @@ function DisplayQuotes() {
       </article>
     );
   }
+
   return (
-    <ul>
+    <ul style={{
+      listStyle: 'none',
+      fontWeight: 'bolder',
+    }}
+    >
       {data.map((quote) => (
-        <li key={quote}>
+        <li
+          key={quote.id}
+        >
+
           {quote.quote}
+          {' '}
+          -
           {' '}
           {quote.author}
         </li>
