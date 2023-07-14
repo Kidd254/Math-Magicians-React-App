d41a2cfce7db80dcfc3ec5a8974477e2e3da9269
Kidd254 <lorenkioko@gmail.com>
2023-07-14 09:20:25 +0300

    add testing for operator.js

--- src/App.test.js
+++ src/App.test.js
@@ -90,3 +90,59 @@ describe('Calculate module', () => {
     expect(calculate(cal, '=').total).toBe('3');
   });
 });
+
+// testa for operate .js
+
+describe('Operate module', () => {
+  test('Test operate sum n1: 6 plus n2: 4', () => {
+    const cal = {
+      next: '6',
+      total: '4',
+      operation: '+',
+    };
+    expect(operate(cal.next, cal.total, cal.operation)).toBe('10');
+  });
+
+  test('Test operate substraction n1: 6 - n2: 4', () => {
+    const cal = {
+      next: '6',
+      total: '4',
+      operation: '-',
+    };
+    expect(operate(cal.next, cal.total, cal.operation)).toBe('2');
+  });
+
+  test('Test operate divition n1: 6 ÷ n2: 3', () => {
+    const cal = {
+      next: '6',
+      total: '3',
+      operation: '÷',
+    };
+    expect(operate(cal.next, cal.total, cal.operation)).toBe('2');
+  });
+
+  test('Test operate multiplication n1: 6 x n2: 4', () => {
+    const cal = {
+      next: '6',
+      total: '4',
+      operation: 'x',
+    };
+    expect(operate(cal.next, cal.total, cal.operation)).toBe('24');
+  });
+
+  test('Test calculator and operator module n1: 23 % n2: 5', () => {
+    const cal = {
+      next: '23',
+      total: '5',
+      operation: '%',
+    };
+    expect(operate(cal.next, cal.total, cal.operation)).toBe('3');
+  });
+
+  test('Unknown operation throws error', () => {
+    const numb1 = 10;
+    const numb2 = 2;
+    const operation = '/';
+    expect(() => operate(numb1, numb2, operation)).toThrowError();
+  });
+});
