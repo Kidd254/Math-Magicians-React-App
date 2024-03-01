1b883d418f688f3e5bb62ceacf901330e375163c
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-01 20:07:51 +0300

    Add tailwind styling to navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,42 +1,38 @@
 import { NavLink } from 'react-router-dom';
-import styles from '../Styles/NavigationBar.css';
 
 function Navbar() {
   return (
-    <nav className={styles.nav}>
-      <h1 className={styles.h1}>Math Magicians</h1>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
+    <nav className="bg-blue-500 p-4 flex flex-col md:flex-row items-center justify-between">
+      <h1 className="text-white text-2xl font-bold md:px-5 mb-2 md:mb-0">
+        Math Magicians
+      </h1>
+      <ul className="flex items-center space-x-4 text-white md:my-0">
+        <li>|</li>
+        <li>
           <NavLink
             to="/"
-            className={({ isActive }) => {
-              const active = isActive ? styles.active : styles.navLink;
-              return active;
-            }}
+            activeClassName="underline"
+            className="hover:underline py-4"
           >
             Home
           </NavLink>
         </li>
         <li>|</li>
-        <li className={styles.li}>
+        <li>
           <NavLink
             to="calculator"
-            className={({ isActive }) => {
-              const active = isActive ? styles.active : styles.navLink;
-              return active;
-            }}
+            activeClassName="underline"
+            className="hover:underline py-4"
           >
             Calculator
           </NavLink>
         </li>
         <li>|</li>
-        <li className={styles.li}>
+        <li>
           <NavLink
             to="quote"
-            className={({ isActive }) => {
-              const active = isActive ? styles.active : styles.navLink;
-              return active;
-            }}
+            activeClassName="underline"
+            className="hover:underline py-4"
           >
             Quote
           </NavLink>
