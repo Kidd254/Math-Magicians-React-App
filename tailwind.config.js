11fae8cfc6314d437611ce79e88ffa2d02c3ce26
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-01 16:56:44 +0300

    Add global styling to container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}',
     './public/index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
