d1d38b23884193cce9bb804d566d0794e0eb3600
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-01 20:06:02 +0300

    ASd tailwind styling to calculator page

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -32,34 +32,36 @@ function Calculator() {
   return (
     <div className="container mx-auto p-4">
       <p className="text-2xl font-bold mb-4">Let&apos;s do some math!</p>
-      <div className="mx-auto w-96 p-4 border rounded-lg bg-gray-800 text-white">
-        <div className="flex items-center justify-end h-16">
-          <Display numberParent={displayNumber} />
-        </div>
-        <div className="grid grid-cols-4 gap-2">
-          <GrayButton string="AC" clickCalc={clickCalc} />
-          <GrayButton string="+/-" clickCalc={clickCalc} />
-          <GrayButton string="%" clickCalc={clickCalc} />
-          <OrangeButton string="÷" first clickCalc={clickCalc} />
+      <div className="mx-auto grid grid-cols-2 gap-4">
+        <div className="w-full p-4 border rounded-lg bg-gray-800 text-white">
+          <div className="flex items-center justify-end h-16">
+            <Display numberParent={displayNumber} />
+          </div>
+          <div className="grid grid-cols-4 gap-2">
+            <GrayButton string="AC" clickCalc={clickCalc} />
+            <GrayButton string="+/-" clickCalc={clickCalc} />
+            <GrayButton string="%" clickCalc={clickCalc} />
+            <OrangeButton string="÷" first clickCalc={clickCalc} />
 
-          <GrayButton string="7" clickCalc={clickCalc} />
-          <GrayButton string="8" clickCalc={clickCalc} />
-          <GrayButton string="9" clickCalc={clickCalc} />
-          <OrangeButton string="x" clickCalc={clickCalc} />
+            <GrayButton string="7" clickCalc={clickCalc} />
+            <GrayButton string="8" clickCalc={clickCalc} />
+            <GrayButton string="9" clickCalc={clickCalc} />
+            <OrangeButton string="x" clickCalc={clickCalc} />
 
-          <GrayButton string="4" clickCalc={clickCalc} />
-          <GrayButton string="5" clickCalc={clickCalc} />
-          <GrayButton string="6" clickCalc={clickCalc} />
-          <OrangeButton string="-" clickCalc={clickCalc} />
+            <GrayButton string="4" clickCalc={clickCalc} />
+            <GrayButton string="5" clickCalc={clickCalc} />
+            <GrayButton string="6" clickCalc={clickCalc} />
+            <OrangeButton string="-" clickCalc={clickCalc} />
 
-          <GrayButton string="1" clickCalc={clickCalc} />
-          <GrayButton string="2" clickCalc={clickCalc} />
-          <GrayButton string="3" clickCalc={clickCalc} />
-          <OrangeButton string="+" clickCalc={clickCalc} />
+            <GrayButton string="1" clickCalc={clickCalc} />
+            <GrayButton string="2" clickCalc={clickCalc} />
+            <GrayButton string="3" clickCalc={clickCalc} />
+            <OrangeButton string="+" clickCalc={clickCalc} />
 
-          <GrayButton string="0" clickCalc={clickCalc} big />
-          <GrayButton string="." clickCalc={clickCalc} />
-          <OrangeButton string="=" clickCalc={clickCalc} />
+            <GrayButton string="0" clickCalc={clickCalc} big />
+            <GrayButton string="." clickCalc={clickCalc} />
+            <OrangeButton string="=" clickCalc={clickCalc} />
+          </div>
         </div>
       </div>
     </div>
